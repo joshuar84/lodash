53a4e62aaa5aea828a4eb3608803478153dc873e
Joshua Rios <1984yearoftherat@gmail.com>
2021-01-18 12:37:40 -1000

    finish pad method

--- _.js
+++ _.js
@@ -5,7 +5,6 @@ const _ = {
         return clampedValue;
     },
     inRange(num, start = 0, end) {
-        
         if (end === undefined) {
             end = start;
             start = 0;
@@ -13,8 +12,8 @@ const _ = {
                 return true;
             } else {
                 return false;
-            };
-        };
+            }
+        }
         if (start > end) {
             let temp = end;
             end = start;
@@ -23,10 +22,39 @@ const _ = {
                 return true;
             } else {
                 return false;
-            };
-        };
-        let isInRange = (start <= num && num < end);
+            }
+        }
+        let isInRange = start <= num && num < end;
         return isInRange;
+    },
+    words(string) {
+        const arrayOfWords = string.split(" ");
+        return arrayOfWords;
+    },
+    pad(string, length) {
+        const sLength = string.length;
+        const difference = length - sLength;
+
+        const spacer = (num) => {
+            let array = [];
+            for (let i = 0; i < num; i++) {
+                array.push(" ");
+            }
+            return array.join("");
+        };
+
+        if (sLength > length) {
+            return string;
+        };
+
+        if (sLength % 2 === 1 || length % 2 === 1) {
+            const strtPad = Math.floor(difference / 2);
+            const endPad = Math.ceil(difference / 2);
+            return spacer(strtPad) + string + spacer(endPad);
+        };
+        
+        const padding = difference / 2;
+        return spacer(padding) + string + spacer(padding);
     },
 };
 
