100e53119dbb3c4fd96f6a8d4d441a069602f6cf
Joshua Rios <1984yearoftherat@gmail.com>
2021-01-18 11:24:33 -1000

    finished inRange method

--- _.js
+++ _.js
@@ -4,14 +4,31 @@ const _ = {
         const clampedValue = Math.min(lowerClampedValue, upper);
         return clampedValue;
     },
-    inRange() {
-
+    inRange(num, start = 0, end) {
+        
+        if (end === undefined) {
+            end = start;
+            start = 0;
+            if (start <= num && num < end) {
+                return true;
+            } else {
+                return false;
+            };
+        };
+        if (start > end) {
+            let temp = end;
+            end = start;
+            start = temp;
+            if (start <= num && num < end) {
+                return true;
+            } else {
+                return false;
+            };
+        };
+        let isInRange = (start <= num && num < end);
+        return isInRange;
     },
 };
-
-
-
-
 
 // Do not write or modify code below this line.
 module.exports = _;
