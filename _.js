1cc9e77fbd26af16fa67eb9e6b980a6a7c64e1ea
Joshua Rios <1984yearoftherat@gmail.com>
2021-01-19 09:51:40 -1000

    finished chunk their way

--- _.js
+++ _.js
@@ -105,6 +105,18 @@ const _ = {
         });
         let droppedArray = this.drop(array, dropNumber)
         return droppedArray;
+    },
+
+    chunk(array, size) {
+        if (size === undefined) {
+            size = 1;
+        };
+        let arrayChunks = [];
+        for (let i = 0; i < array.length; i += size) {
+            let arrayChunk = array.slice(i, i + size);
+            arrayChunks.push(arrayChunk);
+        };
+        return arrayChunks;
     }
 };
 
